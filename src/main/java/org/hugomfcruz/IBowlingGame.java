441bc8e884ff0bfd03712c7d1a4934e2a1846335
hcruz <hugo.cruz@anaplan.com>
2018-10-31 09:02:04 +0000

    Add basic implementation and tests for BowlingGame kata.

---
+++ src/main/java/org/hugomfcruz/IBowlingGame.java
@@ -0,0 +1,9 @@
+package org.hugomfcruz;
+
+public interface IBowlingGame {
+
+    void roll(int pins);
+
+    int getScore();
+
+}
---
+++ src/test/java/org/hugomfcruz/BowlingGameBaseTest.java
@@ -0,0 +1,10 @@
+package org.hugomfcruz;
+
+import org.junit.Before;
+
+public class BowlingGameBaseTest extends BowlingGameTests {
+    @Before
+    public void setUp() {
+        bowlingGame = new BowlingGameBase();
+    }
+}
