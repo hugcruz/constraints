701236310e94aa9824cb80bb718a757f1c1f6dc6
hcruz <hugo.cruz@anaplan.com>
2019-02-15 19:25:04 +0000

    Saner version of no-loops implementation.

--- src/main/java/org/hugomfcruz/BowlingGameNoLoops.java
+++ src/main/java/org/hugomfcruz/BowlingGameNoLoops.java
@@ -1,107 +1,53 @@
 package org.hugomfcruz;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.stream.IntStream;
 
 public class BowlingGameNoLoops implements IBowlingGame {
-    private int[] rolls = new int[21];
-    private int currentRoll = 0;
+    private Deque<Integer> rolls = new ArrayDeque<>();
 
     @Override
     public void roll(int pins) {
-            rolls[currentRoll++] = pins;
+        rolls.add(pins);
     }
 
     @Override
     public int getScore() {
+        return IntStream.range(0, 10) // 10 rounds
+                        .map(i -> readFromQueue())
+                        .sum();
 
-        List<Score> listOfScores =
-                IntStream
-                        .range(0, rolls.length)
-                        .mapToObj(this::scoreFactory)
-                        .collect(Collectors.toList());
-
-        List<Score> filteredScores =
-                IntStream
-                        .range(0, listOfScores.size())
-                        .filter(i -> isValid(i, listOfScores))
-                        .mapToObj(listOfScores::get)
-                        .collect(Collectors.toList());
-
-        List<Score> filteredScoresWithBonusAdjustments =
-                IntStream
-                        .range(0, 10)
-                        .mapToObj(filteredScores::get)
-                        .collect(Collectors.toList());
-
-        return filteredScoresWithBonusAdjustments.stream()
-                        .map(Score::getScore)
-                        .reduce(0, Integer::sum);
     }
 
-    private boolean isValid(int rollIndex, List<Score> listOfScores) {
-        long numberOfCurrentStrikes = listOfScores.subList(0, rollIndex).stream().filter(s -> s instanceof Strike).count();
-        long adjustmentForStrikes = numberOfCurrentStrikes % 2;
+    /**
+     * Calculates the score for each round.
+     * Side-effect: consumes elements from the queue.
+     * @return the score for the round on the head of the queue.
+     */
+    private int readFromQueue() {
+        int score1 = rolls.pop();
+        int score2 = rolls.pop();
 
-        return  (rollIndex + adjustmentForStrikes) % 2 == 0 ||      // every other non-strike counts, with adjustment for the strikes previously scored.
-                listOfScores.get(rollIndex - 1) instanceof Strike;  // Strikes always count on their own
-    }
+        // Strike
+        if(score1 == 10){
 
-    private Score scoreFactory(int rollIndex) {
-        int rollScore1 = getScoreForRoll(rollIndex);
-        int rollScore2 = getScoreForRoll(rollIndex + 1);
-        int rollScore3 = getScoreForRoll(rollIndex + 2);
+            // 10 + 2 next rolls
+            int score3 = rolls.element();
+            rolls.addFirst(score2); // put back second roll
+            return score1 + score2 + score3;
 
-        Score score;
-        if(rollScore1 == 10){
-            score = new Strike();
-        } else if(rollScore1 + rollScore2 == 10){
-            score = new Spare();
+        // Spare
+        } else if (score1 + score2 == 10) {
+
+            // 10 + next roll
+            int score3 = rolls.element();
+            return score1 + score2 + score3;
+
+        //Split
         } else {
-            score = new Split();
-        }
+            return score1 + score2;
 
-        return score.pushResult(rollScore1)
-                    .pushResult(rollScore2)
-                    .pushResult(rollScore3);
-    }
-
-    private int getScoreForRoll(int rollIndex){
-        return rollIndex < rolls.length ? rolls[rollIndex] : 0;
-    }
-
-    private abstract class Score {
-        protected int[] results = new int[3];
-        private int numberOfResults;
-
-        public abstract int getScore();
-
-        protected Score pushResult(int result) {
-            results[numberOfResults] = result;
-            numberOfResults++;
-            return this;
-        }
-    }
-
-    private class Strike extends Score {
-        @Override
-        public int getScore() {
-            return 10 + results[1] + results[2];
-        }
-    }
-
-    private class Spare extends Score {
-        @Override
-        public int getScore() {
-            return 10 + results[2];
-        }
-    }
-
-    private class Split extends Score {
-        @Override
-        public int getScore() {
-            return results[0] + results[1];
         }
     }
 }
